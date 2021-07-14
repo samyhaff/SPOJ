1d9b73c02af0c9d19130b90f465475b6feaf2fe3
Samy Haffoudhi <haffoudhisamy@gmail.com>
2021-07-14 18:25:32 +0200

    first problem

---
+++ life_the_universe_and_everything.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+    while (x != 42)
+    {
+        cout << x << endl;
+        cin >> x;
+    }
+
+    return 0;
+}
