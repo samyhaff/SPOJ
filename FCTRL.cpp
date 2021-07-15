a10653636fbb9fd9a3b526d450e8d43b93a4b1f9
Samy Haffoudhi <haffoudhisamy@gmail.com>
2021-07-15 21:07:52 +0200

    3rd problem

---
+++ FCTRL.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdtr1c++.h> 
+
+using namespace std;
+
+int Z(int n, int k)
+{
+    if (n / k == 0)
+        return 0;
+    return n / k + Z(n, 5 * k);
+}
+
+int main()
+{
+    int t, n;
+    cin >> n;
+
+    for (int i = 0; i < t; i++)
+    {
+        cin >> n;
+        cout << Z(n, 5) << endl;
+    }
+
+    return 0;
+}
